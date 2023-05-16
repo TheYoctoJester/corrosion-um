b3ec3184e2913c3ba1ff2e92475efab1ad5cf93c
TheYoctoJester <jester@theyoctojester.info>
2023-05-16 21:55:29 +0200

    chore: remove dependency on strum
    Signed-off-by: TheYoctoJester <jester@theyoctojester.info>

--- src/mender_update_module.rs
+++ src/mender_update_module.rs
@@ -1,13 +1,11 @@
 pub mod invocation {
 	
 use std::path::PathBuf;
-use std::str::FromStr;
-use strum_macros::EnumString;
 
 const ERR_NOT_ENOUGH_ARGUMENTS: &str = "not enough arguments";
 const ERR_ACTION_NOT_RECOGNIZED: &str = "action string not recognized";
 
-#[derive(Debug, PartialEq, EnumString)]
+#[derive(Debug, PartialEq)]
 pub enum Action {
 	Download,
 	ArtifactInstall,
@@ -33,14 +31,27 @@ pub struct Invocation {
 
 impl Invocation {
 	pub fn build(args: &[String]) -> Result<Invocation, &'static str> {
-		// first try: an information request, because that only requires one argument
+		// we always get two arguments!
 		if args.len() < 3 {
 			return Err(ERR_NOT_ENOUGH_ARGUMENTS)
 		}
 
-		let action: Action = match Action::from_str(&args[1]) {
-			Err(_) => return Err(ERR_ACTION_NOT_RECOGNIZED),
-			Ok(a) => a
+		let action: Action = match args[1].as_str() {
+			"Download" => Action::Download,
+			"ArtifactInstall" => Action::Download,
+			"ArtifactReboot" => Action::Download,
+			"ArtifactVerifyReboot" => Action::Download,
+			"ArtifactCommit" => Action::Download,
+			"Cleanup" => Action::Download,
+			"ArtifactRollback" => Action::Download,
+			"ArtifactRollbackReboot" => Action::Download,
+			"ArtifactFailure" => Action::Download,
+			"SupportsRollback" => Action::Download,
+			"NeedsArtifactReboot" => Action::Download,
+			"SupportsAugmentedArtifacts" => Action::Download,
+			"ListSupportedOriginalTypes" => Action::Download,
+			"PermittedAugmentedHeaders" => Action::Download,
+			_ => return Err(ERR_ACTION_NOT_RECOGNIZED)
 		};
 
 		Ok(Invocation{action, path: PathBuf::from(args[2].clone())})
